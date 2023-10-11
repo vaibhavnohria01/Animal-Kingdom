81a94cae7c5a4e4b381c732e2e7ba5369051910e
Yihang Zhu <u7630167@anu.edu.au>
2023-10-11 21:42:36 +1100

    Rewrote loading data.

--- FaunalEncyclopedia/app/src/main/java/com/g12/faunalencyclopedia/ListActivity.java
+++ FaunalEncyclopedia/app/src/main/java/com/g12/faunalencyclopedia/ListActivity.java
@@ -43,11 +43,5 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-
-
-
-
-
-
     }
 }
